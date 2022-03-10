fbb2178eb01781fd71295250777f7f63da3a806b
remin <rdrgn@users.noreply.github.com>
2022-03-10 13:28:56 +0900

    build: employ esbuild

--- src/index.php
+++ src/index.php
@@ -6,10 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>php-react-example</title>
 
-  <script src="https://unpkg.com/react@17/umd/react.production.min.js" crossorigin></script>
-  <script src="https://unpkg.com/react-dom@17/umd/react-dom.production.min.js" crossorigin></script>
-  <script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script>
-  <script type="text/babel" src="index.js"> </script>
+  <script src="index.js" defer></script>
 </head>
 
 <body>
